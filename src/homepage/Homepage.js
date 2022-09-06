8e877a8db74c3b201f789f21e40bb166ef2ff9e5
Camilla Cortney <camilla.cortney@gmail.com>
2022-09-06 13:42:27 -0700

    created homepage

---
+++ src/homepage/Homepage.js
@@ -0,0 +1,41 @@
+import {useContext} from "react";
+import {Link} from "react-router-dom";
+import UserContext from "../auth/UserContext";
+import { Grid, Typography, Box, Button} from "@mui/material"
+
+const Homepage = () => {
+    const {currentUser} = useContext(UserContext);
+
+    return (
+        <Grid container align='center' spacing={2} direction="column" sx={{mt: 3}}>
+            <Grid item>
+                <Typography variant="h2" fontWeight='fontWeightMedium'>
+                        BUDDY READS
+                </Typography>
+            </Grid>
+            <Grid item>
+                <Typography variant="subtitle1" >
+                    Choose a book. Find a friend. Discuss as you go.
+                </Typography>
+            </Grid>
+            <Grid item>
+            {currentUser ? 
+                <Typography variant="h4" >
+                    Welcome back, {currentUser.firstName} {currentUser.lastName}!
+                </Typography> :
+                <Box >
+                    <Button variant="contained" sx={{mr: 1}}>
+                        <Link style={{textDecoration: "none", color: "white"}} to={`/signup`}>Sign Up</Link>
+                    </Button>
+                    <Button variant="contained" sx={{ml: 1}}>
+                        <Link style={{textDecoration: "none", color: "white"}} to={`/login`}>LOGIN</Link>
+                    </Button>
+                </Box>
+            }
+            </Grid>
+            
+        </Grid>
+    )
+}
+
+export default Homepage;
