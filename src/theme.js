c2d20b8ca0311b8c39d368e042f22603e6bac91f
Camilla Cortney <camilla.cortney@gmail.com>
2022-09-22 21:23:22 -0700

    started custom theme

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#3949ab", 
+            light: "#9fa8da"
+        }, 
+        secondary: {
+            main: "#ffa000"
+        }
+    }
+    
+})
+
+export default theme;
+
+// 
