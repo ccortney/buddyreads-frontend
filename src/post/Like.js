66fcde8fab6ee6d95df6660fa175aae19bdff09e
Camilla Cortney <camilla.cortney@gmail.com>
2022-09-27 20:21:00 -0700

    created and styled Like component

--- src/post/Like.js
+++ src/post/Like.js
@@ -1,38 +1,64 @@
-import { Grid, Button, Typography, Box } from "@mui/material";
-import { useState, useContext, useEffect } from "react";
+import { Checkbox, IconButton, Tooltip, } from "@mui/material";
+import {  useContext, Fragment } from "react";
+import { Favorite, FavoriteBorder, DeleteForever } from "@mui/icons-material";
 import UserContext from "../auth/UserContext";
 
+/** Show Like component
+ *  
+ * Like component is shown and disabled if buddy has liked their post.
+ * Like component is shown if post is revealed
+ *
+ * It is rendered by Post 
+ * 
+ * {Post} -> { Like }
+ */
 
-const Like = ({post, changeLike, liked}) => {
+
+const Like = ({post, changeLike, liked, deletePost}) => {
     const {currentUser} = useContext(UserContext)
 
         if (currentUser.id === post.userId && liked === true) {
             return (
-                <Button disabled onClick={changeLike}>
-                    Liked
-                </Button>
+                <Fragment>
+                    <Tooltip title="Delete">
+                        <IconButton onClick={() => deletePost(post.id)}>
+                            <DeleteForever/>
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Buddy Liked">
+                        <IconButton>
+                            <Checkbox
+                                checked
+                                disabled
+                                checkedIcon={<Favorite sx={{ color: "grey" }} />}
+                            />
+                        </IconButton>
+                    </Tooltip>
+                </Fragment>
             )
         }
         if (currentUser.id === post.userId && liked === false) {
-            return null
-        }
-        if (liked === true) {
             return (
-                <Button variant="contained" onClick={changeLike}>
-                    Liked
-                </Button>
+                <Tooltip title="Delete">
+                    <IconButton onClick={() => deletePost(post.id)}>
+                        <DeleteForever/>
+                    </IconButton>
+                </Tooltip>
             )
         }
-        if (liked === false) {
-            return (
-                <Button variant="outlined" onClick={changeLike}>
-                    Like
-                </Button>
-            )
-        }
-
-    // return {determineLike()}
-    // return <p>Liked</p>
+ 
+        return (
+            <Tooltip title="Like">
+                <IconButton>
+                    <Checkbox
+                        checked = {liked}
+                        onChange={changeLike}
+                        icon={<FavoriteBorder />}
+                        checkedIcon={<Favorite sx={{ color: "error.main" }} />}
+                    />
+                </IconButton>
+            </Tooltip>
+        )
 }
 
 export default Like;
