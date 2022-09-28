d492bd41908f8bf4df10ffa11960df859baeeffe
Camilla Cortney <camilla.cortney@gmail.com>
2022-09-27 20:22:16 -0700

    created and styled PostList component

--- src/post/PostList.js
+++ src/post/PostList.js
@@ -1,13 +1,35 @@
-import { Grid, Button, Typography, Box } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import Post from "./Post";
 
-const PostList = ({posts, progress}) => {
+/** Shows list of posts. 
+ *
+ * Renders Post component for each post in the list.
+ * 
+ * Rendered by Posts component 
+ *
+ * {Posts} -> {PostList} -> { Post, Like }
+ */
+
+const PostList = ({posts, progress, deletePost}) => {
     return (
         <Box>
-            <ul>
-            {posts.map(post => (<Post key={post.id} post={post} progress={progress}/>))}
-            </ul>
+            {posts.length === 0 ? 
+                <Box padding={2} align="center">
+                    <Typography sx={{ textTransform: 'uppercase', fontWeight: 'light' }} variant='body1'>
+                        This buddy read has no posts. Start the discussion!
+                    </Typography> 
+                </Box> 
+            :
+            posts.map(post => (
+                <Post 
+                    key={post.id} 
+                    post={post} 
+                    progress={progress}
+                    deletePost={deletePost}
+                />))
+            }
         </Box>
+
     )
 }
 
