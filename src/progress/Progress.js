b7b269eda418ff2a84c4a956a523b310fe546037
Camilla Cortney <camilla.cortney@gmail.com>
2022-09-27 20:23:06 -0700

    created and styled progress components

--- src/progress/Progress.js
+++ src/progress/Progress.js
@@ -1,18 +1,27 @@
-import { Grid, Button, Typography, Box, linearProgressClasses } from "@mui/material";
-import {useContext, useState, useEffect} from "react"
+import { Stack } from "@mui/material";
+import {useContext, useState, useEffect } from "react"
 import UserContext from "../auth/UserContext";
 import ProgressForm from "./ProgressForm";
 import BuddyReadApi from "../api/api";
-import ErrorAlert from "../common/ErrorAlert";
-import RatingDialog from "../rating/RatingDialog";
+import ProgressCard from "./ProgressCard";
 
+/** Show Progress component. 
+ * 
+ * On mount, loads progress card component for each user. 
+ * Renders ProgressForm for user to update progress
+ *
+ * It is rendered by BuddyReadDetails. 
+ *
+ * BuddyReadDetails -> {Progress} -> { ProgressCard, ProgressForm }
+ */
 
-const Progress = ({progress, setProgress, buddy, buddyRead, bookData, handleRatingOpen}) => {
+const Progress = ({progress, setProgress, buddy, buddyRead, bookData, rating, setRating}) => {
     const {currentUser} = useContext(UserContext);
     const [formData, setFormData] = useState({progress: ''});
     const [formErrors, setFormErrors] = useState([]);
     const [percents, setPercents] = useState({});
-    const [showForm, setShowForm] = useState(true)
+    const [showForm, setShowForm] = useState(false);
+    const [ratingOpen, setRatingOpen] = useState(false);
 
     useEffect(() => {
         setPercents({
@@ -24,6 +33,10 @@ const Progress = ({progress, setProgress, buddy, buddyRead, bookData, handleRati
         }
     }, [progress, bookData.pageCount])
 
+    const handleRatingOpen = () => {
+        setRatingOpen(true);
+    };
+    
     const handleChange = (e) => {
         const {value} = e.target;
         setFormData({progress: value});
@@ -45,6 +58,7 @@ const Progress = ({progress, setProgress, buddy, buddyRead, bookData, handleRati
         try {
             const newProgress = await BuddyReadApi.updateProgress(buddyRead.id, currentUser.id, +formData.progress);
             setProgress(data => ({...data, currentUserProgress: newProgress.progress}))
+            setShowForm(false)
         } catch (errors) {
             setFormErrors(errors);
         }
@@ -57,37 +71,50 @@ const Progress = ({progress, setProgress, buddy, buddyRead, bookData, handleRati
             await BuddyReadApi.changeStatus(buddyRead.id, {status: 'completed'});
         }
         setFormData({progress: ''});
-
     }
-    
+
     return (
-        <div>
-            {formErrors.length ? <ErrorAlert severity='error' messages={formErrors}/> : null } 
-            <Box sx={{ border: 1}}>
-                <Typography variant='subtitle1' sx={{fontWeight: 'light'}}>
-                    Your Progress
-                </Typography>
-                <Typography variant='subtitle1' sx={{fontWeight: 'bold'}}>
-                    Page: {progress.currentUserProgress} |  {percents.currentUser}%
-                </Typography>
-                {showForm ? 
-                    <ProgressForm
-                        formData={formData}
-                        handleChange={handleChange}
-                        handleSubmit={handleSubmit}
-                    /> 
-                    : null
-                } 
-            </Box>
-            <Box sx={{ border: 1}}>
-                <Typography variant='subtitle1' sx={{fontWeight: 'light'}}>
-                    {buddy.firstName}'s Progress
-                </Typography>
-                <Typography variant='subtitle1' sx={{fontWeight: 'bold'}}>
-                Page: {progress.buddyProgress} |  {percents.buddy}%
-                </Typography>
-            </Box>
-        </div> 
+        <Stack direction={{xs: 'row', sm: "column"}} justifyContent="center">
+            <ProgressCard
+                user={currentUser}
+                progress={progress.currentUserProgress}
+                color="secondary.main"
+                percent={percents.currentUser}
+                isCurrentUser={true}
+                setShowForm={setShowForm}
+                rating={rating.currentUserRating}
+                setRating={setRating}
+                ratingOpen={ratingOpen}
+                setRatingOpen={setRatingOpen}
+                handleRatingOpen={handleRatingOpen}
+                bookData={bookData}
+                buddyRead={buddyRead}
+            />
+            <ProgressCard
+                user={buddy}
+                progress={progress.buddyProgress}
+                color="primary.main"
+                percent={percents.buddy}
+                isCurrentUser={false}
+                setShowForm={setShowForm}
+                rating={rating.buddyRating}
+                ratingOpen={ratingOpen}
+                setRatingOpen={setRatingOpen}
+                handleRatingOpen={handleRatingOpen}
+                setRating={setRating}
+                bookData={bookData}
+                buddyRead={buddyRead}
+            />
+            <ProgressForm
+                showForm={showForm}
+                setShowForm={setShowForm}
+                formData={formData}
+                formErrors={formErrors}
+                setFormErrors={setFormErrors}
+                handleChange={handleChange}
+                handleSubmit={handleSubmit}
+            />
+        </Stack>
     )
 }
 
--- src/progress/ProgressForm.js
+++ src/progress/ProgressForm.js
@@ -1,26 +1,46 @@
-import { Grid, Button, TextField, Box} from "@mui/material";
+import { Stack, Button, TextField, Box, Dialog, DialogTitle, DialogActions, DialogContent} from "@mui/material";
+import ErrorAlert from "../common/ErrorAlert";
 
+/** Progress Form component
+ *
+ * Displays form to update progress
+ * Submitting the form calls the BuddyReadAPI to update progress and
+ * updated page and percents are rendered in the Progress component
+ *  
+ * Rendered by Progress component
+ *
+ * {Progress} -> {ProgressForm}
+ */
 
-const ProgressForm = ({formData, handleChange, handleSubmit}) => {
-
+const ProgressForm = ({showForm, setShowForm, formData, formErrors, setFormErrors, handleChange, handleSubmit}) => {
+    const handleClose = () => {
+        setShowForm(false);
+        setFormErrors([])
+    }
     return (
-        <Grid container align='center' sx={{justifyContent:'center'}}>
+        <Dialog open={showForm} onClose={handleClose}>
+        <DialogTitle>Update Your Progress</DialogTitle>
+        <DialogContent>
             <form onSubmit={handleSubmit}>
-                <Box sx={{ display: 'flex', justifyContent:  'center' }}>
-                    <TextField
-                        label="Page"
-                        id="progress"
-                        name="progress"
-                        value={formData.progress}
-                        onChange = {handleChange}
-                        sx={{ m: 1, width: '10ch' }}
-                    />
-                    <Box sx={{display: 'flex', alignItems: 'center',  m: 1}}>
-                        <Button variant="contained" type="submit">Update</Button>
-                    </Box>
-                </Box>
-            </form>
-        </Grid>
+                <Stack direction="row" justifyContent="center">
+                 <Box sx={{ display: 'flex', justifyContent:  'center' }}>
+                     <TextField
+                         label="Page"
+                         id="progress"
+                         name="progress"
+                         value={formData.progress}
+                         onChange = {handleChange}
+                         sx={{ m: 1, width: '10ch' }}
+                     />
+                 </Box>
+                 <DialogActions>
+                        <Button type="submit">Update</Button>
+                </DialogActions>
+                </Stack>
+             </form>
+             {formErrors.length ? <ErrorAlert severity='error' messages={formErrors}/> : null } 
+        </DialogContent>
+      </Dialog>
     )
 }
 
